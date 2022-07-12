fedb64bfa2926abc6c5ca1e54a649254a5bd5c61
Prozer1 <dev.prozer@gmail.com>
2022-07-12 14:48:08 +0200

    update worker ssl

--- celery_settings.py
+++ celery_settings.py
@@ -2,7 +2,7 @@ import os
 
 task_serializer = 'json'
 accept_content = ['json', 'pickle']
-broker_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-result_backend = os.getenv('REDIS_URL', 'redis://localhost:6379')
+broker_url = os.getenv('REDIS_TLS_URL', 'redis://localhost:6379')
+result_backend = os.getenv('REDIS_TLS_URL', 'redis://localhost:6379')
 task_track_started = True
 redis_socket_timeout = 600
